9222af3cc4a832549f60eccbbc24cea271831dd5
visiter-1998 <775061802@qq.com>
2019-11-19 12:58:40 +0800

    connect with mysql driver

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 
 @app.route('/')
 def hello():
 	return 'hello world'
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:gx_MySQL@localhost:3306/test'
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+#
+# db=SQLAlchemy(app)
+# #manager = Manager(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ sqldrive.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:gx_MySQL@localhost:3306/test'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+
+db=SQLAlchemy()
+
+class User(db.Model):
+    __tablename__='myuser'
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(20))
